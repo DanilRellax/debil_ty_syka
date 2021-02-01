eb42e3a51d991c8da23b88e5050db49fefa19024
DanilRellax <77833594+DanilRellax@users.noreply.github.com>
2021-02-01 08:37:35 +0200

    Add files via upload

---
+++ precache-manifest.d40c4c51df7eb10ee1315756f8fa1815.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f791a717cd442369c95eb76731a6dacc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "69f0cbc116a5e74cdae7",
+    "url": "./static/css/2.55851008.chunk.css"
+  },
+  {
+    "revision": "92a070f9b0a34e664555",
+    "url": "./static/css/main.59ab86c4.chunk.css"
+  },
+  {
+    "revision": "69f0cbc116a5e74cdae7",
+    "url": "./static/js/2.c70c09b6.chunk.js"
+  },
+  {
+    "revision": "317baa17d5ed5b192aa3dddcf99334da",
+    "url": "./static/js/2.c70c09b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a070f9b0a34e664555",
+    "url": "./static/js/main.e857bbb9.chunk.js"
+  },
+  {
+    "revision": "19563eab0ff9f49f5af8",
+    "url": "./static/js/runtime-main.4814125e.js"
+  },
+  {
+    "revision": "bf9a1c17f3ddb72cffaec36368a4e825",
+    "url": "./static/media/Component 18.bf9a1c17.svg"
+  },
+  {
+    "revision": "4a736e27d2ddd50d64b456a6585efab2",
+    "url": "./static/media/Component 19.4a736e27.svg"
+  },
+  {
+    "revision": "4bf8ce4221b1eeed8e34e1abdc6b54d8",
+    "url": "./static/media/Component 20.4bf8ce42.svg"
+  },
+  {
+    "revision": "bd8313439abe352ce475007019387ca8",
+    "url": "./static/media/DurkaCoin.bd831343.svg"
+  },
+  {
+    "revision": "53031f49f5b7bfd4f5e1b6fde8719997",
+    "url": "./static/media/Star.53031f49.svg"
+  },
+  {
+    "revision": "9f0f1030ca8259f9ff5f7684fbad9b77",
+    "url": "./static/media/Star45.9f0f1030.svg"
+  },
+  {
+    "revision": "83aaa5ddd297db41bc42080e171c2b57",
+    "url": "./static/media/adventure.83aaa5dd.png"
+  },
+  {
+    "revision": "91d7f1384e5b64bfe7c530c327f0304e",
+    "url": "./static/media/billie.91d7f138.png"
+  },
+  {
+    "revision": "19da893f4dab35d4429a0aff3bc682b3",
+    "url": "./static/media/bronzeBox.19da893f.svg"
+  },
+  {
+    "revision": "2b858efc63e65ab00cb77c6c74391e46",
+    "url": "./static/media/dataCenter.2b858efc.svg"
+  },
+  {
+    "revision": "f1c434ddca300f84f14df50d479c6d83",
+    "url": "./static/media/dc45.f1c434dd.svg"
+  },
+  {
+    "revision": "1b969e618dc2ce99d618990d78a0624c",
+    "url": "./static/media/donut.1b969e61.png"
+  },
+  {
+    "revision": "2695438e4c2102c19c343319ddf3f942",
+    "url": "./static/media/durak10.2695438e.svg"
+  },
+  {
+    "revision": "21e4ebc2295361de0e5f61eb29e90e8c",
+    "url": "./static/media/durka.21e4ebc2.png"
+  },
+  {
+    "revision": "969059b52b03f5a93d0cac73deb7da34",
+    "url": "./static/media/films.969059b5.png"
+  },
+  {
+    "revision": "6a30299509298ddd9f66c25c267699f0",
+    "url": "./static/media/goldBox.6a302995.svg"
+  },
+  {
+    "revision": "a5f2f27a665edc7afc136f5e03c8af14",
+    "url": "./static/media/graphicsСard.a5f2f27a.svg"
+  },
+  {
+    "revision": "26caf96912566160fcafc5814d981843",
+    "url": "./static/media/joker.26caf969.png"
+  },
+  {
+    "revision": "ac805c08bdd3de208c825942b3f725ef",
+    "url": "./static/media/klunka.ac805c08.png"
+  },
+  {
+    "revision": "3512c7e40a8fed7a94e9bf865066dd6a",
+    "url": "./static/media/kolpak.3512c7e4.svg"
+  },
+  {
+    "revision": "9a7bd8f9e4c9ef60204ab270e1a87497",
+    "url": "./static/media/kotex.9a7bd8f9.png"
+  },
+  {
+    "revision": "4f53c0977299a616c0c9ab0d9aaceab7",
+    "url": "./static/media/malibu.4f53c097.png"
+  },
+  {
+    "revision": "6110850b4e5c2c31791c6f6e7ebf5dd7",
+    "url": "./static/media/mysticalBox.6110850b.svg"
+  },
+  {
+    "revision": "beb716f3745f27686b47f512499d2216",
+    "url": "./static/media/naruto.beb716f3.png"
+  },
+  {
+    "revision": "1a13d5aaa1bf69338e77ab7bc8604e5d",
+    "url": "./static/media/ono.1a13d5aa.png"
+  },
+  {
+    "revision": "8b59c1948ec4f3fb6e3284e82719e026",
+    "url": "./static/media/popugai.8b59c194.png"
+  },
+  {
+    "revision": "dd2f6bcb015336a705c7158209afbea2",
+    "url": "./static/media/quantumPC.dd2f6bcb.svg"
+  },
+  {
+    "revision": "95edba73597a0394d199c5c7f629ad81",
+    "url": "./static/media/serverDC.95edba73.svg"
+  },
+  {
+    "revision": "e5f3c6ef3e829d10e98db5ad69380a84",
+    "url": "./static/media/silverBox.e5f3c6ef.svg"
+  },
+  {
+    "revision": "625a1d19ff731c8b69ac644ee410e76a",
+    "url": "./static/media/simpsons.625a1d19.png"
+  },
+  {
+    "revision": "bd48e1d67a6a2829078e0f81e9bba70c",
+    "url": "./static/media/spyingApp.bd48e1d6.jpg"
+  },
+  {
+    "revision": "cb241ebf974763ba9eb479975f649e33",
+    "url": "./static/media/superPC.cb241ebf.svg"
+  }
+]);
